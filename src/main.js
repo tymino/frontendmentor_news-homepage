ee1320758223bc0fe1a2a4d640cc5a4833b756af
Aleksey <d_team-lex@bk.ru>
2023-02-22 18:06:12 +0300

    1

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import './assets/global.scss';
+
 import { createApp } from 'vue';
 import App from './App.vue';
 
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,10 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'Frontend Mentor | News homepage',
+    },
+  },
 });
