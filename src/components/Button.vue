e7f71b12d67d9bc68f5056d1738ab2236ab40131
Aleksey <d_team-lex@bk.ru>
2023-03-14 18:34:43 +0300

    style Button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -12,5 +12,20 @@ export default {
 
 <style lang="scss" scoped>
 .button {
+  padding: 12px 22px;
+  
+  background: var(--color-soft-red);
+  border: none;
+
+  color: var(--color-off-white);
+  font-weight: var(--font-bold);
+  letter-spacing: 2px;
+  text-transform: uppercase;
+
+  transition: background-color var(--transition-delay);
+
+  &:hover {
+    background: var(--color-very-dark-blue);
+  }
 }
 </style>
