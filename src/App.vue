0365e4de846df3229683c5eed104911df7a54b51
Aleksey <d_team-lex@bk.ru>
2023-03-17 16:47:25 +0300

    save

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="content">
-    <Header />
-    <div class="content__body">
-      <Main class="content__body-main" />
+    <div class="content__header">
+      <Header />
     </div>
-    <div class="content__aside"></div>
+    <div class="content__main">
+      <Main />
+    </div>
+    <div class="content__aside">
+      <Aside />
+    </div>
     <div class="content__bottom"></div>
   </div>
 </template>
@@ -12,26 +16,46 @@
 <script>
 import Header from '@/components/Header.vue';
 import Main from '@/components/Main.vue';
+import Aside from '@/components/Aside.vue';
 
 export default {
   name: 'App',
   components: {
     Header,
     Main,
+    Aside,
   },
 };
 </script>
 
-<style>
-#app {
-  display: flex;
-  justify-content: center;
-}
-
+<style lang="scss" scoped>
 .content {
+  display: grid;
+  grid-template-rows: repeat(4, auto);
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+  grid-template-areas:
+    'a a a'
+    'b b c'
+    'b b c'
+    'd d d';
   margin-top: 40px;
   width: 100%;
   max-width: 1112px;
+
+  &__header {
+    grid-area: a;
+    margin-bottom: 40px;
+  }
+  &__main {
+    grid-area: b;
+  }
+  &__aside {
+    grid-area: c;
+  }
+  &__bottom {
+    grid-area: d;
+  }
 }
 
 @media (max-width: 640px) {
---
+++ src/components/Aside.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="aside">
+    <div class="aside__title">new</div>
+    <div class="aside__body" v-for="item in items" :key="item.title">
+      <div class="aside__body-title">{{ item.title }}</div>
+      <div class="aside__body-body">{{ item.body }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'c-aside',
+  setup() {
+    const items = [
+      {
+        title: 'Hydrogen VS Electric Cars',
+        body: 'Will hydrogen-fueled cars ever catch up to EVs?',
+      },
+      {
+        title: 'The Downsides of AI Artistry',
+        body: 'What are the possible adverse effects of on-demand AI image generation?',
+      },
+      {
+        title: 'Is VC Funding Drying Up?',
+        body: 'Private funding by VC firms is down 50% YOY. We take a look at what that means.',
+      },
+    ];
+
+    return { items };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.aside {
+  background: var(--color-very-dark-blue);
+  &__title {
+    color: var(--color-soft-orange);
+  }
+  
+  &__body {
+    &-title {
+    }
+    &-body {
+    }
+  }
+}
+</style>
