9f0e6201dc377a958c381a2436f3a9e2254d196a
Aleksey <d_team-lex@bk.ru>
2023-03-17 17:01:40 +0300

    style Main component

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -8,14 +8,18 @@
       <img src="@/assets/images/image-web-3-desktop.jpg" alt="web-3" />
     </picture>
 
-    <div class="main__title">The Bright Future of Web 3.0?</div>
-    <div class="main__describe">
-      We dive into the next evolution of the web that claims to put the power of
-      the platforms back into the hands of the people. But is it really
-      fulfilling its promise?
-    </div>
+    <div class="main__wrapper">
+      <div class="main__title">The Bright Future of Web 3.0?</div>
+      <div class="main__describe">
+        <div class="main__describe-text">
+          We dive into the next evolution of the web that claims to put the
+          power of the platforms back into the hands of the people. But is it
+          really fulfilling its promise?
+        </div>
 
-    <Button class="main__button">read more</Button>
+        <Button class="main__describe-button">read more</Button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -32,9 +36,9 @@ export default {
 
 <style lang="scss" scoped>
 .main {
-  display: grid;
-  grid-template-rows: repeat(1fr, 5);
-  grid-template-columns: repeat(1fr 2);
+  display: flex;
+  flex-direction: column;
+  
 
   &__picture {
     & > img {
@@ -43,9 +47,35 @@ export default {
       display: block;
     }
   }
-  &__title {
+
+  &__wrapper {
+    display: grid;
+    grid-template-rows: 1fr;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 20px;
+    margin-top: 20px;
   }
+
+  &__title {
+    padding-right: 20px;
+    color: var(--color-very-dark-blue);
+    font-size: 3.3rem;
+    font-weight: var(--font-ex-bold);
+    line-height: 3.3rem;
+  }
+
   &__describe {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
+    padding-right: 10px;
+
+    // &-text {
+    // }
+
+    // &-button {
+    // }
   }
 }
 </style>
